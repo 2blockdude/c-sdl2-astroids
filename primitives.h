40f4766fbea1ec29b1e2b3e68881a0af48c58e19
Anthony Cobb <2blockdude@gmail.com>
2021-12-11 12:39:31 -0500

    space tabs

--- primitives.h
+++ primitives.h
@@ -33,22 +33,22 @@ struct ellipse
 };
 
 // float polygons
-int draw_polygon								(SDL_Renderer *renderer, const struct polygon *p);
-int draw_polygon_filled					   (SDL_Renderer *renderer, const struct polygon *p);
+int draw_polygon                       (SDL_Renderer *renderer, const struct polygon *p);
+int draw_polygon_filled                (SDL_Renderer *renderer, const struct polygon *p);
 
 // build float polygons
-struct polygon *create_polygon			(float *input_vectors, int nsides, float x, float y, float angle);
-struct polygon *create_reg_polygon	   (int nsides, float x, float y, float angle, float radius);
-struct polygon *create_rand_polygon	   (int nsides, float x, float y, float angle, float max_radius, float min_radius, float angle_offset);
+struct polygon *create_polygon         (float *input_vectors, int nsides, float x, float y, float angle);
+struct polygon *create_reg_polygon     (int nsides, float x, float y, float angle, float radius);
+struct polygon *create_rand_polygon    (int nsides, float x, float y, float angle, float max_radius, float min_radius, float angle_offset);
 struct polygon *create_copy_polygon    (struct polygon *p);
 
 // transform float polygon
-int polygon_rebuild							(struct polygon *p);
-int polygon_translate						(struct polygon *p, float x, float y);
-int polygon_set_angle						(struct polygon *p, float angle);
-int polygon_set_scale						(struct polygon *p, float scale_x, float scale_y);
+int polygon_rebuild                    (struct polygon *p);
+int polygon_translate                  (struct polygon *p, float x, float y);
+int polygon_set_angle                  (struct polygon *p, float angle);
+int polygon_set_scale                  (struct polygon *p, float scale_x, float scale_y);
 
 // free float polygon
-void free_polygon							   (struct polygon *p);
+void free_polygon                      (struct polygon *p);
 
 #endif
